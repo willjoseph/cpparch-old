48fb9042bb5a5b1f35df59dc572d8b7898a82d47
smallpileofgibs <smallpileofgibs@174230dc-fbae-11de-8037-3961252a365c>
2010-05-13 18:21:36 +0000

    updated tests
    git-svn-id: https://cpparch.googlecode.com/svn/trunk@264 174230dc-fbae-11de-8037-3961252a365c

--- cppparse/test/test_lexer.cpp
+++ cppparse/test/test_lexer.cpp
@@ -424,29 +424,3 @@ struct LexIterator: public context_type::iterator_type
 	{
 	}
 };
-
-
-
-#if 0
-template<typename T>
-struct Blah
-{
-	typedef typename T::template X<T::value> Type fe;
-};
-#endif
-
-#if 0
-#include "boost\spirit\home\support\iterators\detail\combine_policies.hpp"
-#endif
-
-#if 0
-#include <boost/integer_fwd.hpp>
-#endif
-
-#if 0
-#include <boost/wave/util/interpret_pragma.hpp>
-#endif
-
-#if 0
-#include <boost/spirit/home/classic/tree/common.hpp>
-#endif
