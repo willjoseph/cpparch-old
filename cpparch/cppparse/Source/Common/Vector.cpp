31b7031c8e99b7628fe82881a037e16d37ce6f34
smallpileofgibs@gmail.com <smallpileofgibs@gmail.com@174230dc-fbae-11de-8037-3961252a365c>
2013-10-09 19:22:49 +0000

    investigation of unique-vector
    git-svn-id: https://cpparch.googlecode.com/svn/trunk@654 174230dc-fbae-11de-8037-3961252a365c

--- cpparch/cppparse/Source/Common/Vector.cpp
+++ cpparch/cppparse/Source/Common/Vector.cpp
@@ -15,3 +15,41 @@ void test()
 	Vector tmp2 = tmp;
 }
 
+#if 0 // experimental
+#include <set>
+
+typedef std::vector<int> Vector;
+typedef std::set<Vector> UniqueVectors;
+
+template<typename A>
+UniqueVectors::iterator insert(UniqueVectors& elements, const std::vector<int, A>& value)
+{
+	UniqueVectors::iterator i = elements.lower_bound(value); // first element not less than value
+	if(i != elements.end()
+		&& !elements.key_comp()(value, *i)) // if value is not less than lower bound
+	{
+		// lower bound is equal to value
+		return i;
+	}
+	i = elements.insert(i, Vector());
+	(*i).insert(value.begin(), value.end()); // copy the value
+	return i;
+}
+
+void test2()
+{
+	typedef std::vector<int, DebugAllocator<int> > Vector2;
+	Vector2 tmp;
+	tmp.reserve(2);
+	tmp.push_back(0);
+	tmp.push_back(1);
+
+	UniqueVectors elements;
+#if 0
+	UniqueVectors::iterator i = elements.insert(tmp).first;
+#else
+	UniqueVectors::iterator i = insert(elements, tmp);
+#endif
+}
+#endif
+
